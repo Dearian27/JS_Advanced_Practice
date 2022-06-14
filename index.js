11e1616981d4c8528908373e7ba4bb4d13c61d42
Dearian <dearian2706@gmail.com>
2022-06-14 22:24:15 +0300

    function closure?

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-console.log('f')
-const root = document.getElementById('root');
+const root = document.getElementById('root')
+ 
+ 
+ const createDomain = (domain) => {
+     return function(url){
+        return `http://${url}.${domain}`
+     }
+ }
 
-// const introduce = (floors) => {
-//     root.innerHTML =  `<<h2>this ${floors}-storey house was built in ${this.yearOfBuild}</h2>`;
-// }
+  const createName = createDomain('com');
 
-const house1 = {
-    color: "white",
-    yearOfBuild: 2020,
-    // logThis: (floors) => { console.log( `color is ${this.color}`)},
-    introduce: function(floors){
-        root.innerHTML =  root.innerHTML + ` <br> <<h2>this ${floors}-storey house was built in ${this.yearOfBuild}</h2>`;
-    }
-}
-const house2 = {
-    color: "yellow",
-    yearOfBuild: 2018,
-}
-house1.introduce.bind(house1, 3)();
-house1.introduce.apply(house2, [2]);
-house1.introduce.call(house1, 1);
+ root.textContent = createName("dearian");
