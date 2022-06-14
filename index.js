dd6297483e91fe9b191ef797902b4bd861cce6f8
Dearian <dearian2706@gmail.com>
2022-06-14 22:57:38 +0300

    myTask1

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const root = document.getElementById('root')
  const person1 = { name: 'Denys', age: 16, job: 'Frontend'}
  const person2 = { name: 'Serhiy', age: 16, job: 'lazing'} 
 
- const logPerson = () => {
+ function logPerson() {
     return console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
  }
 
  const bind = (person, logPerson) => {
-    person.logPerson = logPerson;
-    person.logPerson.bind(person);
-     return logPerson();
+    person.logPerson = logPerson.bind(person);
+     return person.logPerson();
  }
