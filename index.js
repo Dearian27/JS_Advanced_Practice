e24cb0b0da11a7ce46842b613547ea49a4bb60f0
Dearian <dearian2706@gmail.com>
2022-06-24 22:38:45 +0300

    promise2

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const root = document.getElementById('root')
  
- const person1 = { name: 'Denys', age: 16, job: 'Frontend'}
- const person2 = { name: 'Serhiy', age: 16, job: 'lazing'} 
+console.log('request data...');
 
- function logPerson() {
-    return console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
- }
-
- const bind = (data, fn) => {  //1)аргумент: об'єкт 2)аргумент: функція
-    return function(...args){  // використання оператора 'spread' для передання будь-якої к-сті елементів
-        fn.apply(data, args)  // використання  метода 'apply' для того щоб позначити елемент під this та передати аргументи у вигляді масива
-    }
- }
-
- bind(person1, logPerson)()
- bind(person2, logPerson)()
+const prom = new Promise(function(resolve, reject) {
+   setTimeout(function(){
+      console.log('preparing data...');
+      const backendData = {
+         server: "aws",
+         port: 3000,
+         status: 'working'
+      }
+      resolve(backendData)
+   }, 2000)
+})
+prom.then((data) => {
+   return new Promise((resolve, reject) =>{
+      setTimeout(() => {
+         data.modified = true;
+         resolve(data)
+      }, 2000)
+   })
+}).then(receivedData => {
+   console.log('received data.', receivedData)
+   return receivedData
+}).then(clientData => {
+   clientData.fromPromise = true
+   console.log(clientData)
+   return clientData
+}).then(data => {
+   console.log(data)
+}).catch(error => console.error('ERROR:', error)) // if reject
+  .finally(() => console.log('finally')) // in any case
