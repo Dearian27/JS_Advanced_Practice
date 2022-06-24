8798752e4041fbe506a8ac911665f9eb87b5eca2
Dearian <dearian2706@gmail.com>
2022-06-24 22:50:26 +0300

    promise3

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const root = document.getElementById('root')
  
-console.log('request data...');
-
-const prom = new Promise(function(resolve, reject) {
-   setTimeout(function(){
-      console.log('preparing data...');
-      const backendData = {
-         server: "aws",
-         port: 3000,
-         status: 'working'
-      }
-      resolve(backendData)
-   }, 2000)
+const sleep = ms => new Promise(resolve => {
+   setTimeout(() => resolve(), ms)
 })
-prom.then((data) => {
-   return new Promise((resolve, reject) =>{
-      setTimeout(() => {
-         data.modified = true;
-         resolve(data)
-      }, 2000)
-   })
-}).then(receivedData => {
-   console.log('received data.', receivedData)
-   return receivedData
-}).then(clientData => {
-   clientData.fromPromise = true
-   console.log(clientData)
-   return clientData
-}).then(data => {
-   console.log(data)
-}).catch(error => console.error('ERROR:', error)) // if reject
-  .finally(() => console.log('finally')) // in any case
+
+.then(() => console.log('After 5 seconds'))
+
+Promise.all([sleep(2000), sleep(3000)]) // wait for all promises
+.then(() => {
+   console.log('all promises done')
+})
+
+Promise.race([sleep(2000), sleep(3000)]) // wait for first promise
+.then(() => {
+   console.log('race promise')
+})
