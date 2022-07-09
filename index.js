7ef41fc7334e5db6e9a94ac5488240ba7e6da3af
Dearian <dearian2706@gmail.com>
2022-07-09 16:01:20 +0300

    ES6+_classes1

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
 const root = document.getElementById('root')
  
-const person = Object.create({
-   calcAge() {
-      return new Date().getFullYear() - this.birthYear
-   }
-}, {
-   name: {
-      value: 'Den',  // keyword "value"
-      enumerable: true, // is numerable in 'for' cycles
-      writable: true, // can change value
-      configurable: true // can delete
-   },
-   birthYear: {
-      value: 2005,
-      enumerable: false,
-      writable: false,
-      configurable: false 
-   },
-   age: {
-      get(){ // give value to element using function
-         return new Date().getFullYear() - this.birthYear
-      },
-      set(){ // func doing when element of object will change
-         document.body.style.background = "red"
-         console.log('set age', value)
-      }
-   }
-})
-
-console.log(person)
-
-person.name = 'not a Den'; // change prop 'name' (writable: true) 
-console.log(person)
-
-delete person.name; // delete prop 'name' (configurable: true)
-console.log(person)
-
-person.name = 'Den'; // create prop 'name' again and set value
-console.log(person)
-
-for (let key in person) {
-   if(person.hasOwnProperty(key)) // do not walk in _proto_
+class Animal {
+   constructor(options)
    {
-      console.log('Key', key, person[key])
+      this.name = options.name
+      this.age = options.age
+      this.hasTale = options.hasTale
+   }
+   voice()
+   {
+      console.log("Animal!!!")
+   }
+} 
+
+class Dog extends Animal{
+   constructor(options)
+   {
+      super(options)
+      this.color = options.color
+   }
+
+   voice()
+   {
+      super.voice() // call method voice from class Animal
+      console.log("ruff!!!")
    }
 }
+const Jack_Rassel = new Dog( {name: 'Jack', age: 3, hasTale: true, color: 'white'} )
